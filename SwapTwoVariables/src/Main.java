eb9b8c95868a6b6194525a6cab945033e1bc6654
Pawan Maurya <110979745+Pawan2505@users.noreply.github.com>
2023-11-05 10:01:51 +0530

    Swap file changed.

--- SwapTwoVariables/src/Main.java
+++ SwapTwoVariables/src/Main.java
@@ -1,32 +1,20 @@
+
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        y = ++x + x++ + x
-        System.out.println("a = "+x);
+//        int a = 5;
+//        int b = 10;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a = "+a);
         System.out.println("b = "+b);
     }
 }
-
-
-//import java.util.Scanner;
-//public class Main {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-////        int a = 5;
-////        int b = 10;
-//        int a = sc.nextInt();
-//        int b = sc.nextInt();
-//        a=a+b;
-//        b=a-b;
-//        a=a-b;
-//        System.out.println("a = "+a);
-//        System.out.println("b = "+b);
-//    }
-//}
 //import java.util.Scanner;
 //public class Main {
 //    public static void main(String[] args) {
